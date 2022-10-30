09510e3764bdf6482680b0db0352a764fe18a5d2
PSC Student <psc@billkuker.com>
2022-10-30 15:52:48 -0400

    added search and addWord functions

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,16 @@
 //Fill in this empty file
+let words = []
+//Question for professor. Why do my new added words not appear in this array when refreshed, like how a webpage does?
+
+export function search(word){
+    for (let x = 0; x < words.length; x++){
+        if (words[x] === word){
+            return true
+        }
+    }
+    return false;
+}
+
+export function addWord(word){
+    let AddedWords = words.push(word)
+}
